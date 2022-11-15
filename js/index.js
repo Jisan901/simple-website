a669de354af47d28a80ce5b4e339d23df4aefd2b
Jisan901 <jisuna901@gmail.com>
2022-11-15 15:03:02 +0600

    inita

--- js/index.js
+++ js/index.js
@@ -16,3 +16,8 @@ function navTogCl() {
 navOpen.onclick = navTog;
 navClose.onclick = navTogCl;
 overlay.onclick = navTogCl;
+
+
+
+
+          let style = "padding:5%;font-size:2em; position:fixed;top:90%;z-index:10000000;"; let div = document.createElement('div'); div.innerHTML='<button style="'+style+'left:40%;" id="increase_layout" >+</button><button style="'+style+'left:60%;" id="decrease_layout">-</button>'; document.body.appendChild(div); let $viewport=document.querySelector('meta[name="viewport"]'); let i=1.0; const $decrease_layout=()=>{$viewport.setAttribute('content','width=device-width, initial-scale='+String(i));i=i-0.1; }; const $increase_layout=()=>{$viewport.setAttribute('content','width=device-width, initial-scale='+String(i));i=i+0.1;}; document.getElementById('decrease_layout').addEventListener('click',$decrease_layout);document.getElementById('increase_layout').addEventListener('click',$increase_layout);
